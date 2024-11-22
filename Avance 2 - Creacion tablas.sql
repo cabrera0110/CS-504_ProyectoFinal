85338369627535852a9497fc503449886826824e
cabrera0110 <da_cabrera0110@live.com>
2024-11-22 13:13:45 -0600

    V3.2

--- Avance 2 - Creacion tablas.sql
+++ Avance 2 - Creacion tablas.sql
@@ -1,74 +1,77 @@
 -- Crear el esquema boxlyServicios
-CREATE SCHEMA boxlyServicios;
+ALTER SESSION SET CONTAINER = XEPDB1;
+CREATE USER boxlyServicios IDENTIFIED BY oracle123;
+GRANT CONNECT, RESOURCE TO boxlyServicios;
+ALTER SESSION SET CURRENT_SCHEMA = BOXLYSERVICIOS;
 
 -- Crear la tabla Cliente
 CREATE TABLE Cliente (
-    IDCliente int primary key not null,
-    NombreCliente varchar(30) not null,
-    CorreoCliente varchar(30) not null,
-    TelefonoCliente varchar(30),
-    DireccionCliente varchar(100),
-    NumeroCasillero int
+    IDCliente NUMBER PRIMARY KEY NOT NULL,
+    NombreCliente VARCHAR2(30) NOT NULL,
+    CorreoCliente VARCHAR2(30) NOT NULL,
+    TelefonoCliente VARCHAR2(30),
+    DireccionCliente VARCHAR2(100),
+    NumeroCasillero NUMBER
 );
 
 -- Crear la tabla Casillero
 CREATE TABLE Casillero (
-    IDCasillero int primary key not null,
-    IDCliente int,
-    DireccionCasillero varchar(100)
+    IDCasillero NUMBER PRIMARY KEY NOT NULL,
+    IDCliente NUMBER,
+    DireccionCasillero VARCHAR2(100)
 );
 
 -- Crear la tabla Paquete
 CREATE TABLE Paquete (
-    IDPaquete int primary key not null,
-    IDCasillero int,
-    Descripcion varchar(50),
-    ValorDeclarado decimal(10,2),
-    PesoPaquete decimal(10,2),
-    EstadoPaquete varchar(30),
-    FechaRecepcion date,
-    FechaEntrega date
+    IDPaquete NUMBER PRIMARY KEY NOT NULL,
+    IDCasillero NUMBER,
+    Descripcion VARCHAR2(50),
+    ValorDeclarado NUMBER(10, 2),
+    PesoPaquete NUMBER(10, 2),
+    EstadoPaquete VARCHAR2(30),
+    FechaRecepcion DATE,
+    FechaEntrega DATE
 );
 
 -- Crear la tabla Prealerta
 CREATE TABLE Prealerta (
-    IDPrealerta int primary key not null,
-    IDPaquete int,
-    FechaEnvio date,
-    Courier varchar(50),
-    NumeroRastro int,
-    CostoTraida number(10, 2)
+    IDPrealerta NUMBER PRIMARY KEY NOT NULL,
+    IDPaquete NUMBER,
+    FechaEnvio DATE,
+    Courier VARCHAR2(50),
+    NumeroRastro NUMBER,
+    CostoTraida NUMBER(10, 2)
 );
 
 -- Crear la tabla Factura
 CREATE TABLE Factura (
-    IDFactura int primary key not null,
-    IDUsuario int,
-    MontoTotal number(10, 2),
-    Fecha date,
-    EstadoPago varchar(30),
-    MetodoPago varchar(30)
+    IDFactura NUMBER PRIMARY KEY NOT NULL,
+    IDUsuario NUMBER,
+    MontoTotal NUMBER(10, 2),
+    Fecha DATE,
+    EstadoPago VARCHAR2(30),
+    MetodoPago VARCHAR2(30)
 );
 
 -- Crear la tabla Notificaciones
 CREATE TABLE Notificaciones (
-    IDNotificacion int primary key not null,
-    IDPaquete int not null,
-    Mensaje varchar(255) not null,
-    FechaNotificacion DATE not null
+    IDNotificacion NUMBER PRIMARY KEY NOT NULL,
+    IDPaquete NUMBER NOT NULL,
+    Mensaje VARCHAR2(255) NOT NULL,
+    FechaNotificacion DATE NOT NULL
 );
 
 -- Asignar la clave foránea FK_IDcliente a la tabla Casillero
-ALTER TABLE Casillero ADD CONSTRAINT FK_IDcliente foreign key (IDCliente) references Cliente(IDCliente);
+ALTER TABLE Casillero ADD CONSTRAINT FK_IDcliente FOREIGN KEY (IDCliente) REFERENCES Cliente(IDCliente);
 
 -- Asignar la clave foránea FK_IDpaquete a la tabla Prealerta
-ALTER TABLE Prealerta ADD CONSTRAINT FK_IDpaquete foreign key (IDPaquete) references Paquete(IDPaquete);
+ALTER TABLE Prealerta ADD CONSTRAINT FK_IDpaquete FOREIGN KEY (IDPaquete) REFERENCES Paquete(IDPaquete);
 
 -- Asignar la clave foránea FK_IDusuario a la tabla Factura
-ALTER TABLE Factura ADD CONSTRAINT FK_IDusuario foreign key (IDUsuario) references Cliente(IDCliente);
+ALTER TABLE Factura ADD CONSTRAINT FK_IDusuario FOREIGN KEY (IDUsuario) REFERENCES Cliente(IDCliente);
 
--- Asignar la clave foránea FK_IDpaquete a la tabla Notificaciones
-ALTER TABLE Notificaciones ADD CONSTRAINT FK_IDpaquete foreign key (IDPaquete) references Paquete(IDPaquete);
+-- Asignar la clave foránea FK_IDpaquete_Notificaciones a la tabla Notificaciones
+ALTER TABLE Notificaciones ADD CONSTRAINT FK_IDpaquete_Notificaciones FOREIGN KEY (IDPaquete) REFERENCES Paquete(IDPaquete);
 
 -- Asignar la clave foránea FK_IDCasillero a la tabla Paquete
 ALTER TABLE Paquete ADD CONSTRAINT FK_IDCasillero FOREIGN KEY (IDCasillero) REFERENCES Casillero(IDCasillero);
